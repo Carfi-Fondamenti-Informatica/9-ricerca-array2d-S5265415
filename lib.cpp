829b9849a59fb9244bc3fc7af19f61368fc4f882
S5265415 <101595525+S5265415@users.noreply.github.com>
2022-05-15 16:24:31 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,16 @@
-
+int posizione(char a[20][10], char b[20]){
+    for(int i=0;i<10;i++){
+        int j=0;
+        int c=0;
+        while(j<20){
+            if(a[j][i]==b[j]){
+                c++;
+            }
+            j++;
+        }
+        if(c==20){
+            return i;
+        }
+    }
+    return -1;
+}
